d84834cbe209e4271dc01fb8aaaee86affdb6056
armelmbia10 <armelmbia08@gmail.com>
2025-03-20 10:15:50 +0100

    mise à jour deploy

--- script/push_to_huggingface.py
+++ script/push_to_huggingface.py
@@ -4,7 +4,7 @@ from huggingface_hub import HfApi, Repository
 hf_token = "YOUR_HUGGING_FACE_TOKEN"
 
 # Nom du repository sur Hugging Face
-repo_name = "your-username/your-repo-name"
+repo_name = "armelmbia/CC_GROUPE_1"
 
 # Chemin vers votre repository local
 local_repo_path = "c:/Users/HP/Documents/GitHub/CC_GROUPE_1"
@@ -18,5 +18,5 @@ repo = Repository(local_dir=local_repo_path, clone_from=repo_name, use_auth_toke
 
 # Ajouter tous les fichiers et pousser les changements
 repo.git_add()
-repo.git_commit("Initial commit")
+repo.git_commit("Dernier commit")
 repo.git_push()
